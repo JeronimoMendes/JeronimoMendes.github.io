0afd3ee5f1d96817cc18b2edc61a5bee27e2d5c1
Jerónimo Mendes <39437433+JeronimoMendes@users.noreply.github.com>
2022-01-04 23:20:43 +0000

    Switch to page hosted on university

---
+++ resources/js/index.js
@@ -0,0 +1,39 @@
+
+// Loading animation
+var loading = document.getElementsByClassName("loading")
+while (loading.length > 0) {
+    console.log(loading[0]);
+    loading[0].classList.add("loaded");
+    loading[0].classList.remove("loading");
+}
+
+
+// Changes navbar background
+var navbar = document.getElementsByClassName("navbar");
+window.onscroll = function() {
+    console.log("Scrolling");
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+        navbar[0].classList.add('scrolled');
+    } else {
+        navbar[0].classList.remove('scrolled');
+    }
+}
+
+
+new TypeIt("#bio", {
+    speed: 50,
+    loop: true
+})
+    .type("computer science student")
+    .pause(700)
+    .delete()
+    .type("programmer")
+    .pause(700)
+    .delete()
+    .type("photography enthusiast")
+    .pause(700)
+    .delete()
+    .type("amateur chess player")
+    .pause(700)
+    .go();
